f3bccebd3cd472d520c224ad0c07be02c503a22b
Yonas Bahre <yonasbahre@ufl.edu>
2022-05-09 22:27:15 -0400

    Connect bot to server and add scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+require("dotenv").config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client( {intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} );
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+    console.log("Mission control, our bot is go flight!");
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("message", (message: any) => {
+    if (message.author.bot) return;
+    message.channel.send("silence");
+});
